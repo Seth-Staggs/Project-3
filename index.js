4d5f4ce47db080c6e3b4ed521742e8cc24d8f7c3
Seth Staggs <sethstaggs2@gmail.com>
2021-03-10 22:34:19 -0600

    finished setting up server and added User and Post models

--- index.js
+++ index.js
@@ -2,18 +2,33 @@ const { ApolloServer } = require("apollo-server");
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
+const Post = require('./Models/Post');
+
 const { MONGODB } = require('./config.js');
 
 //Graphql
 const typeDefs = gql`
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
     type Query{
-        sayHi: String!
+        getPosts: [Post]
     }
 `
 
 const resolvers = {
     Query: {
-        sayHi: () => "Hello World!"
+        async getPosts(){
+            try{
+                const posts = await Post.find();
+                return posts;
+            } catch(err){
+                throw new Error(err);
+            }
+        }
     }
 }
 
