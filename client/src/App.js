96298d056ea129f32ddde27a1ceefc7dd4076857
Karen Acosta <kar3n.acosta@gmail.com>
2021-03-18 22:44:06 -0500

    adding route components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div><h1>Hello World</h1></div>
+    <Router>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/login' component={Login}/>
+      <Route exact path='/register' component={Register}/>
+    </Router>
   );
 }
 
